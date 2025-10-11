1453a541460fcd7c89cb547aba5b8d7f4e9fcf4a
FIstNCISBall <lulubraun1999@gmail.com>
2025-10-11 00:46:25 +0000

    das kommt

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "project-6425387820896908123",
-  "appId": "1:430542533425:web:8c5b3e2d1d0c4a4a4e6e2f",
-  "storageBucket": "project-6425387820896908123.appspot.com",
   "apiKey": "AIzaSyDF18Jqr7s0s64iT_tcaA0AL2D0ebfeZ-A",
   "authDomain": "project-6425387820896908123.firebaseapp.com",
-  "messagingSenderId": "430542533425"
+  "projectId": "project-6425387820896908123",
+  "storageBucket": "project-6425387820896908123.appspot.com",
+  "messagingSenderId": "430542533425",
+  "appId": "1:430542533425:web:8c5b3e2d1d0c4a4a4e6e2f"
 };
